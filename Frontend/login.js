7e72c4e9791d491ed99dc8e7c6afad28d741e0a7
Jean-François Mullier <j-f.mullier@hotmail.fr>
2023-11-29 18:57:10 +0100

    ajout du dossier Frontend

---
+++ Frontend/login.js
@@ -0,0 +1,33 @@
+//Récupération des valeurs du formulaire:
+
+
+
+//Sauvegarde des éléments du formulaire login sur l'API:
+
+
+function saveLoginUser() {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const emailTag = document.getElementById("emailAddress").value;
+        const passwordTag = document.getElementById("password").value;
+        console.log("Pas de rechargement de la page")
+        console.log(emailTag);
+        console.log(passwordTag);
+
+        const loginValue = {
+            email: emailTag,
+            password : passwordTag,
+        }
+
+        fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "accept": "application/json"
+                },
+            body: JSON.stringify(loginValue),
+        })
+    });
+}
+saveLoginUser();
