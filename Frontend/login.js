5d44a4e1e29ecad12af3f788aff1babc9865c147
Jean-François Mullier <j-f.mullier@hotmail.fr>
2023-11-30 21:52:26 +0100

    ajout des fichiers login.js et log.txt

--- Frontend/login.js
+++ Frontend/login.js
@@ -1,7 +1,32 @@
+function goToMainPage() {
+    //Bascule vers la page index.html quand l'utilisateur est logé
+    let connexionBtn = document.getElementById("connexion");
+    connexionBtn.addEventListener("click", (event) => {
+        document.location.href="index.html"
+    })         
+}
+
+function validerEmail(emailTag) {
+    //Permet de vérifier si l'email est au format souhaité:
+    const emailTag = document.getElementById("emailAddress").value;
+    let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+");
+    if(emailRegExp.test(emailTag)) {
+        return true;
+    } else {
+        alert("L'adresse mail n'est pas au bon format.");
+        return false;
+    }  
+};
+
+function errorEmailPassword(emailTag,passwordTag) {
+    //Affiche un message d'alerte si la combinaison email-password est incorrect
+    if (emailTag !== "sophie.bluel@test.tld" && passwordTag !== "S0phie" ) {
+        return alert("La combinaison email - mot de passe n'est pas valide.");
+    }
+};
+
 
 //Sauvegarde des éléments du formulaire login sur l'API:
-
-
 function saveLoginUser() {
     const form = document.querySelector("form");
     form.addEventListener("submit", (event) => {
@@ -17,6 +42,8 @@ function saveLoginUser() {
             email: emailTag,
             password : passwordTag,
         }
+        validerEmail(emailTag);
+        errorEmailPassword(emailTag,passwordTag);
 
         fetch("http://localhost:5678/api/users/login", {
             method: "POST",
