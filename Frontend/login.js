758cf7368dd4ef3eed086305a4ae5b9bc1f52485
Jean-François Mullier <j-f.mullier@hotmail.fr>
2023-12-03 11:02:02 +0100

    maj login.js et Log.txt

--- Frontend/login.js
+++ Frontend/login.js
@@ -6,7 +6,7 @@ function goToMainPage() {
     })         
 }
 
-function validerEmail(emailTag) {
+function validerEmail() {
     //Permet de vérifier si l'email est au format souhaité:
     const emailTag = document.getElementById("emailAddress").value;
     let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+");
@@ -20,7 +20,7 @@ function validerEmail(emailTag) {
 
 function errorEmailPassword(emailTag,passwordTag) {
     //Affiche un message d'alerte si la combinaison email-password est incorrect
-    if (emailTag !== "sophie.bluel@test.tld" && passwordTag !== "S0phie" ) {
+    if (emailTag !== "sophie.bluel@test.tld" || passwordTag !== "S0phie" ) {
         return alert("La combinaison email - mot de passe n'est pas valide.");
     }
 };
@@ -44,8 +44,9 @@ function saveLoginUser() {
         }
         validerEmail(emailTag);
         errorEmailPassword(emailTag,passwordTag);
-
-        fetch("http://localhost:5678/api/users/login", {
+        //pour récupérer les informations de connexion (email, password, userId, token d'authentification)
+        let url = "http://localhost:5678/api/users/login"
+        fetch(url, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -53,6 +54,16 @@ function saveLoginUser() {
                 },
             body: JSON.stringify(loginValue),
         })
-    });
-}
+        .then((reponse) =>reponse.json())
+        .then((data) => {
+            console.log(data);
+            window.localStorage.setItem(data);
+        })
+        .catch(error => console.log("Erreur : " + error));
+
+
+        }
+
+    
+)};
 saveLoginUser();
